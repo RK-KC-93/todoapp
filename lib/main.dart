e7a4aa5aa025c8d5d78141cd0d301a2f6515977f
RK-KC-93 <ronnikkc0@gmail.com>
2024-11-20 11:50:39 +0545

    create a custom splash screen with static image brackground and branding

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todoapp/pages/home_page.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async{
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
 
   //initilize the hive
   await Hive.initFlutter();
@@ -13,8 +17,27 @@ void main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+    initilization();
+  }
+
+  void initilization() async{
+    await Future.delayed(const Duration(seconds: 2));
+    FlutterNativeSplash.remove();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,5 +47,4 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue,),
     );
   }
-
 }
